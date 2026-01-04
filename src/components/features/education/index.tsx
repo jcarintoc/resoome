b1ca80025305c29a5afb8db5f548407e6bfa60de
jcarintoc <carlotaarintoc@gmail.com>
2026-01-04 23:57:08 +0800

    feat: add education form

--- src/components/features/education/index.tsx
+++ src/components/features/education/index.tsx
@@ -1,5 +1,307 @@
-const index = () => {
-  return <div>Education</div>;
+import { Input } from "@/components/ui/input";
+import { useFormContext, useFieldArray } from "react-hook-form";
+import type { ResumeValues } from "@/@types/resume";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Plus, Trash2 } from "lucide-react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import EmptyData from "@/components/ui/empty-data";
+import { toast } from "sonner";
+import AlertPopup from "@/components/ui/alert-popup";
+import { Checkbox } from "@/components/animate-ui/components/radix/checkbox";
+
+const EducationSection = () => {
+  const { control, setValue, watch } = useFormContext<ResumeValues>();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "education",
+  });
+
+  const educationValues = watch("education");
+
+  const onHandleRemove = (index: number) => {
+    remove(index);
+    toast.success("Education removed successfully");
+  };
+
+  if (fields.length === 0) {
+    return (
+      <EmptyData
+        title="No Education"
+        description="You haven't added any education yet."
+        buttonLabel="Add Education"
+        onClick={() =>
+          append({
+            schoolName: "",
+            city: "",
+            country: "",
+            program: "",
+            graduationMonth: "",
+            graduationYear: "",
+            showAdditionalInfo: false,
+            additionalInfo: {
+              gpa: 0,
+              awards: "",
+              extracurricular: "",
+            },
+          })
+        }
+      />
+    );
+  }
+
+  return (
+    <div className="space-y-6">
+      {fields.map((item, index) => (
+        <Card
+          key={item.id}
+          className="gap-2 p-3 hover:ring-2 hover:ring-primary"
+        >
+          <CardHeader className="flex flex-row items-start justify-between space-y-0">
+            <Badge>Education {index + 1}</Badge>
+            <AlertPopup
+              title="Delete Education"
+              description="Are you sure you want to delete this education?"
+              cancelLabel="Cancel"
+              acceptLabel="Delete"
+              onAccept={() => onHandleRemove(index)}
+              variant={"destructive"}
+              size={"icon"}
+              acceptClassName="bg-red-600 hover:bg-red-700 text-white"
+            >
+              <Trash2 className="size-4" />
+            </AlertPopup>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* School Name */}
+              <FormField
+                control={control}
+                name={`education.${index}.schoolName`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>School Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="University of the Philippines"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Program */}
+              <FormField
+                control={control}
+                name={`education.${index}.program`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Program/Degree</FormLabel>
+                    <FormControl>
+                      <Input placeholder="BS Computer Science" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* City */}
+              <FormField
+                control={control}
+                name={`education.${index}.city`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Quezon City" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Country */}
+              <FormField
+                control={control}
+                name={`education.${index}.country`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Philippines" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* Graduation Month */}
+              <FormField
+                control={control}
+                name={`education.${index}.graduationMonth`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Graduation Month</FormLabel>
+                    <FormControl>
+                      <Input placeholder="May" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Graduation Year */}
+              <FormField
+                control={control}
+                name={`education.${index}.graduationYear`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Graduation Year</FormLabel>
+                    <FormControl>
+                      <Input placeholder="2024" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Show Additional Info */}
+            {/* Show Additional Info */}
+            <FormField
+              control={control}
+              name={`education.${index}.showAdditionalInfo`}
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-center gap-2 space-y-0">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={(checked) => {
+                        field.onChange(checked);
+                        if (!checked) {
+                          setValue(`education.${index}.additionalInfo.gpa`, 0);
+                          setValue(
+                            `education.${index}.additionalInfo.awards`,
+                            ""
+                          );
+                          setValue(
+                            `education.${index}.additionalInfo.extracurricular`,
+                            ""
+                          );
+                        }
+                      }}
+                    />
+                  </FormControl>
+                  <FormLabel className="font-normal cursor-pointer">
+                    Additional Info
+                  </FormLabel>
+                </FormItem>
+              )}
+            />
+
+            {educationValues?.[index]?.showAdditionalInfo && (
+              <>
+                {/* GPA Score */}
+                <FormField
+                  control={control}
+                  name={`education.${index}.additionalInfo.gpa`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>GPA</FormLabel>
+                      <FormControl>
+                        <Input type="number" placeholder="3.8" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Awards */}
+                <FormField
+                  control={control}
+                  name={`education.${index}.additionalInfo.awards`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Awards{" "}
+                        <Badge variant="secondary">Comma separated</Badge>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Summa Cum Laude, Dean's Lister"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Extracurricular */}
+                <FormField
+                  control={control}
+                  name={`education.${index}.additionalInfo.extracurricular`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        Extracurricular Activities{" "}
+                        <Badge variant="secondary">Comma separated</Badge>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Student Council, Coding Club"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
+          </CardContent>
+        </Card>
+      ))}
+
+      <Button
+        type="button"
+        variant="outline"
+        className="w-full border-dashed"
+        onClick={() =>
+          append({
+            schoolName: "",
+            city: "",
+            country: "",
+            program: "",
+            graduationMonth: "",
+            graduationYear: "",
+            showAdditionalInfo: false,
+            additionalInfo: {
+              gpa: 0,
+              awards: "",
+              extracurricular: "",
+            },
+          })
+        }
+      >
+        <Plus className="h-4 w-4" />
+        Add Education
+      </Button>
+    </div>
+  );
 };
 
-export default index;
+export default EducationSection;
