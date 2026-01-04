d73e809007d7ee7a79a0cdcef54e8e488a9154f2
jcarintoc <carlotaarintoc@gmail.com>
2026-01-04 11:17:16 +0800

    fix: nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import { ThemeTogglerButton } from "./animate-ui/components/buttons/theme-toggle
 
 const Navbar = () => {
   return (
-    <header className="relative bg-background/15 backdrop-blur-lg border p-1 rounded-xl">
+    <header className="relative border bg-sidebar p-1 rounded-xl">
       <nav className="flex items-center justify-between gap-4">
         <div className="flex items-center justify-center bg-primary h-9 text-background px-4 rounded-lg">
           <h1 className="font-bold">Resoome</h1>
