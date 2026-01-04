fb822b69c5f2dfb667dc47335744d567280aae4d
jcarintoc <carlotaarintoc@gmail.com>
2026-01-04 11:14:50 +0800

    feat: add theme toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import Navbar from "@/components/navbar";
 import ResumeBuilder from "@/components/features/resume-builder";
+import { StripeBgGuides } from "@/components/ui/stripe-bg-guides";
 
 export function App() {
   return (
-    <div>
-      <div className="max-w-7xl mx-auto p-2 space-y-20">
+    <div className="relative min-h-screen">
+      <StripeBgGuides
+        columnCount={12}
+        animated={true}
+        animationDuration={8}
+        glowColor="hsl(var(--primary))"
+        randomize={true}
+        randomInterval={3000}
+        contained={true}
+        className="bg-gray-50 dark:bg-background"
+      />
+
+      <div className="z-10 max-w-7xl mx-auto p-2 space-y-20">
         <Navbar />
         <ResumeBuilder />
       </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,14 @@
-import { Button } from "./ui/button";
+import { ThemeTogglerButton } from "./animate-ui/components/buttons/theme-toggler";
 
 const Navbar = () => {
   return (
-    <header className="border border-primary/75 p-1 rounded-lg">
+    <header className="relative bg-background/15 backdrop-blur-lg border p-1 rounded-xl">
       <nav className="flex items-center justify-between gap-4">
-        <div className="flex items-center justify-center bg-primary h-9 text-background px-4 rounded-md">
+        <div className="flex items-center justify-center bg-primary h-9 text-background px-4 rounded-lg">
           <h1 className="font-bold">Resoome</h1>
         </div>
 
-        <Button>Theme toggle</Button>
+        <ThemeTogglerButton modes={["light", "dark"]} />
       </nav>
     </header>
   );
